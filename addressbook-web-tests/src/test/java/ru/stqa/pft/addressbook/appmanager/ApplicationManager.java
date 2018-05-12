6d8be11bc060ae6b9c073c12b9e2151c6f121f72
ivanovaelena33@gmail.com <ivanovaelena33@gmail.com>
2018-05-12 15:08:58 +0300

    Замена наследования на делегирование и создание двухуровневой архитектуры для ContactTests

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ApplicationManager.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ApplicationManager.java
@@ -1,7 +1,9 @@
 package ru.stqa.pft.addressbook.appmanager;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.firefox.FirefoxOptions;
+import ru.stqa.pft.addressbook.model.AddContactData;
 
 import java.util.concurrent.TimeUnit;
 
@@ -16,7 +18,7 @@ public class ApplicationManager {
   public void init() {
     wd = new FirefoxDriver(new FirefoxOptions().setLegacy(true));
     wd.manage().timeouts().implicitlyWait(60, TimeUnit.SECONDS);
-    wd.get("http://localhost/addressbook/group.php");
+    wd.get("http://localhost/addressbook/");
     groupHelper = new GroupHelper(wd);
     navigationHelper = new NavigationHelper(wd);
     sessionHelper = new SessionHelper(wd);
@@ -34,5 +36,47 @@ public class ApplicationManager {
 
   public NavigationHelper getNavigationHelper() {
     return navigationHelper;
+  }
+
+  public void returnToHomePage() {
+    wd.findElement(By.linkText("home page")).click();
+  }
+
+  public void enterAddNewContact() {
+    wd.findElement(By.xpath("//div[@id='content']/form/input[21]")).click();
+  }
+
+  public void fillAddNewContactForm(AddContactData addContactData) {
+    wd.findElement(By.name("firstname")).click();
+    wd.findElement(By.name("firstname")).clear();
+    wd.findElement(By.name("firstname")).sendKeys(addContactData.getFirstname());
+    wd.findElement(By.name("middlename")).click();
+    wd.findElement(By.name("middlename")).clear();
+    wd.findElement(By.name("middlename")).sendKeys(addContactData.getMiddlename());
+    wd.findElement(By.name("home")).click();
+    wd.findElement(By.name("home")).clear();
+    wd.findElement(By.name("home")).sendKeys(addContactData.getHomePhone());
+    wd.findElement(By.name("mobile")).click();
+    wd.findElement(By.name("mobile")).clear();
+    wd.findElement(By.name("mobile")).sendKeys(addContactData.getMobile());
+    wd.findElement(By.name("email")).click();
+    wd.findElement(By.name("email")).clear();
+    wd.findElement(By.name("email")).sendKeys("p.ivanov@gmail.com");
+  }
+
+  public void gotoAddNewContact() {
+    wd.findElement(By.linkText("add new")).click();
+  }
+
+  public void goToHomePage() {
+    wd.findElement(By.linkText("home")).click();
+  }
+
+  public void deleteContact() {
+    wd.findElement(By.xpath("//div[@id='content']/form[2]/input[2]")).click();
+  }
+
+  public void editContact() {
+    wd.findElement(By.xpath("//table[@id='maintable']/tbody/tr[2]/td[8]/a/img")).click();
   }
 }
---
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/AddContactTests.java
@@ -0,0 +1,15 @@
+package ru.stqa.pft.addressbook.tests;
+
+import org.testng.annotations.Test;
+import ru.stqa.pft.addressbook.model.AddContactData;
+
+public class AddContactTests extends TestBase {
+
+  @Test
+  public void testAddContact() {
+    app.gotoAddNewContact();
+    app.fillAddNewContactForm(new AddContactData("Petr", "Ivanov", "+79265410230", "84956236520"));
+    app.enterAddNewContact();
+    app.returnToHomePage();
+  }
+}
