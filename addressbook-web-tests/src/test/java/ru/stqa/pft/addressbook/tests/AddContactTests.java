e0f727b09b3738c7480237f300eb386261911653
ivanovaelena33@gmail.com <ivanovaelena33@gmail.com>
2018-05-27 00:50:11 +0300

    Добавлена анонимная функция в AddContactTests

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/AddContactTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/AddContactTests.java
@@ -12,19 +12,12 @@ public class AddContactTests extends TestBase {
   @Test
   public void testAddContact() {
     List<ContactData> before = app.getContactHelper().getContactList();
-    ContactData contact = new ContactData("Petr", "Ivanov", "Ivanovich", "+79265410230", "84956236520", "p.ivanov@gmail.com", "test1");
+    ContactData contact = new ContactData("Ivan", "Ivanov", "Ivanovich", "+79265410230", "84956236520", "p.ivanov@gmail.com", "test1");
     app.getContactHelper().createContact(contact);
     List<ContactData> after = app.getContactHelper().getContactList();
     Assert.assertEquals(after.size(), before.size() + 1);
 
-
-    int max = 0;
-    for (ContactData c : after) {
-      if (c.getId() > max) {
-        max = c.getId();
-      }
-    }
-    contact.setId(max);
+    contact.setId(after.stream().max((o1, o2) -> Integer.compare(o1.getId(), o2.getId())).get().getId());
     before.add(contact);
     Assert.assertEquals(new HashSet<Object>(before), new HashSet<Object>(after));
   }
