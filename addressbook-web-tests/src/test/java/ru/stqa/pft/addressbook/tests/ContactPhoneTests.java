ae5b0d3e7fb7308c770872d9810859f5ca08c1f6
ivanovaelena33@gmail.com <ivanovaelena33@gmail.com>
2018-06-03 00:26:44 +0300

    Удалены лишние строки кода

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactPhoneTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactPhoneTests.java
@@ -30,33 +30,23 @@ public class ContactPhoneTests extends TestBase {
     ContactData contactInfoFromEditForm = app.contact().infoFromEditForm(contact);
 
     assertThat(contact.getAllPhones(), equalTo(mergePhones(contactInfoFromEditForm)));
-    //assertThat(contact.getMobilePhone(), equalTo(cleaned(contactInfoFromEditForm.getMobilePhone())));
-    //assertThat(contact.getWorkPhone(), equalTo(cleaned(contactInfoFromEditForm.getWorkPhone())));
     assertThat(contact.getAllEmails(), equalTo(mergeEmails(contactInfoFromEditForm)));
-    //assertThat(contact.getAddress(), equalTo(mergeAddress(contactInfoFromEditForm)));
     assertThat(contact.getAddress(), equalTo(contactInfoFromEditForm.getAddress()));
-      }
-
-      /*
-  private String mergeAddress(ContactData contact) {
-    return Arrays.asList(contact.getAddress())
-            .stream().filter((s) -> ! s.equals("")).map(ContactPhoneTests::cleaned)
-            .collect(Collectors.joining("\n"));
-  }*/
+  }
 
   private String mergePhones(ContactData contact) {
     return Arrays.asList(contact.getHomePhone(), contact.getMobilePhone(), contact.getWorkPhone())
-            .stream().filter((s) -> ! s.equals("")).map(ContactPhoneTests::cleaned)
+            .stream().filter((s) -> !s.equals("")).map(ContactPhoneTests::cleaned)
             .collect(Collectors.joining("\n"));
   }
 
   private String mergeEmails(ContactData contact) {
     return Arrays.asList(contact.getEmail(), contact.getEmail2(), contact.getEmail3())
-            .stream().filter((s) -> ! s.equals("")).map(ContactPhoneTests::cleaned)
+            .stream().filter((s) -> !s.equals("")).map(ContactPhoneTests::cleaned)
             .collect(Collectors.joining("\n"));
   }
 
   public static String cleaned(String phone) {
-    return phone.replaceAll("\\s","").replaceAll("[-()]", "");
+    return phone.replaceAll("\\s", "").replaceAll("[-()]", "");
   }
 }
