ffdaef4018b1e83cbe674e456567834dc791e076
ivanovaelena33@gmail.com <ivanovaelena33@gmail.com>
2018-06-07 21:40:12 +0300

    В AddContactTests прописан путь к файлу с картинкой

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/model/ContactData.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/model/ContactData.java
@@ -1,5 +1,7 @@
 package ru.stqa.pft.addressbook.model;
 
+import java.io.File;
+
 public class ContactData {
   private int id = Integer.MAX_VALUE;
   private String firstname;
@@ -15,6 +17,8 @@ public class ContactData {
   private String email2;
   private String email3;
   private String group;
+  private File photo;
+
 
   public ContactData withId(int id) {
     this.id = id;
@@ -86,6 +90,11 @@ public class ContactData {
     return this;
   }
 
+  public ContactData withPhoto(File photo) {
+    this.photo = photo;
+    return this;
+  }
+
   public int getId() {
     return id;
   }
@@ -140,6 +149,10 @@ public class ContactData {
 
   public String getGroup() {
     return group;
+  }
+
+  public File getPhoto() {
+    return photo;
   }
 
   @Override
--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/AddContactTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/AddContactTests.java
@@ -3,6 +3,9 @@ package ru.stqa.pft.addressbook.tests;
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.ContactData;
 import ru.stqa.pft.addressbook.model.Contacts;
+
+import java.io.File;
+
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.MatcherAssert.assertThat;
 
@@ -11,11 +14,24 @@ public class AddContactTests extends TestBase {
   @Test
   public void testAddContact() {
     Contacts before = app.contact().all();
-    ContactData contact = new ContactData().withFirstname("Ivan").withLastname("Ivanov").withMiddlename("Ivanovich").withMobilePhone("+79265410230").withHomePhone("84956236520").withEmail("p.ivanov@gmail.com").withGroup("test1");
+    File photo = new File("src/test/resources/tree.png");
+    ContactData contact = new ContactData().withFirstname("Ivan").withLastname("Ivanov").withMiddlename("Ivanovich")
+            .withMobilePhone("+79265410230").withHomePhone("84956236520")
+            .withEmail("p.ivanov@gmail.com").withGroup("test1").withPhoto(photo);
     app.contact().create(contact);
     assertThat(app.contact().count(), equalTo(before.size() + 1));
     Contacts after = app.contact().all();
     assertThat(after, equalTo
             (before.withAdded(contact.withId(after.stream().mapToInt((g) -> g.getId()).max().getAsInt()))));
   }
+
+/*
+  @Test
+  public void testCurrentDir() {
+    File currentDir = new File(".");
+    System.out.println(currentDir.getAbsolutePath());
+    File photo = new File("src/test/resources/tree.png");
+    System.out.println(photo.getAbsolutePath());
+    System.out.println(photo.exists());
+  }*/
 }
