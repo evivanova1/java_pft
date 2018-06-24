c01170064f6a7d070f7241eaf3376455bf66de1b
ivanovaelena33@gmail.com <ivanovaelena33@gmail.com>
2018-06-24 13:14:55 +0300

    Изменения в RegistrationHelper

--- mantis-tests/src/test/java/ru/stqa/pft/mantis/appmanager/RegistrationHelper.java
+++ mantis-tests/src/test/java/ru/stqa/pft/mantis/appmanager/RegistrationHelper.java
@@ -19,6 +19,6 @@ public class RegistrationHelper extends HelperBase{
     wd.get(confirmationLink);
     type(By.name("password"), password);
     type(By.name("password_confirm"), password);
-    click(By.cssSelector("input[value='Изменить учетную запись']"));
+    click(By.cssSelector("span.bigger-110"));
   }
 }
