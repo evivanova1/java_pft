78fa7859168a20c05a3d1289448974207e0c00df
ivanovaelena33@gmail.com <ivanovaelena33@gmail.com>
2018-07-03 23:26:22 +0300

    .

--- rest-sample/src/test/java/ru/stqa/pft/rest/RestAssuredTests.java
+++ rest-sample/src/test/java/ru/stqa/pft/rest/RestAssuredTests.java
@@ -33,8 +33,7 @@ public class RestAssuredTests {
     String json = RestAssured.get("http://demo.bugify.com/api/issues.json").asString();
     JsonElement parsed = new JsonParser().parse(json);
     JsonElement issues = parsed.getAsJsonObject().get("issues");
-    return new Gson().fromJson(issues, new TypeToken<Set<Issue>>() {
-    }.getType());
+    return new Gson().fromJson(issues, new TypeToken<Set<Issue>>() {}.getType());
   }
 
   private int createIssue(Issue newIssue) {
